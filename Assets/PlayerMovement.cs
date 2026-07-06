440f0e94475e205f75e182acb59affea4052d3a9
0V3RR1D3 <spieringlogan@gmail.com>
2026-07-06 16:06:37 -0700

    Updated Gravity System

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,59 +4,43 @@ public class PlayerController : MonoBehaviour
 {
     
     private Rigidbody2D rb;
-    private Vector2 moveInput;
-    
     private float gravityStrengthX = 0;
-
     private float gravityStrengthY = 0;
-
-    private float playerVelocity = 1;
-    public float gravityModifierX;
-    public float gravityModifierY;
+    public float globalGravityModifier = 1;
     void Start()
     {
         // Get the Rigidbody2D component attached to the player
         rb = GetComponent<Rigidbody2D>();
+        rb.gravityScale = 0;
     }
 
     
- void Update()
-{
-    Physics2D.gravity = new Vector2(gravityStrengthX, gravityStrengthY); 
-    // Check if the Up Arrow is pressed
-    if (Input.GetKeyDown(KeyCode.UpArrow))
-    {
-        gravityStrengthX = 0 * gravityModifierX;
-        gravityStrengthY = 1 * gravityModifierY;
-        //FreezeX();
-    }
-    if (Input.GetKeyDown(KeyCode.DownArrow))
-    {
-        gravityStrengthX = 0 * gravityModifierX;
-        gravityStrengthY = -1 * gravityModifierY;
-        //FreezeX();
-    }
-    if (Input.GetKeyDown(KeyCode.LeftArrow))
-    {
-        gravityStrengthX = -1 * gravityModifierX;
-        gravityStrengthY = 0 * gravityModifierY;
-        //FreezeY();
-    }
-    if (Input.GetKeyDown(KeyCode.RightArrow))
-    {
-        gravityStrengthX = 1 * gravityModifierX;
-        gravityStrengthY = 0 * gravityModifierY;
-        //FreezeY();
+void Update() {
+    // Updates Global Gravity When Arrow Keys are pressed
+    if (Input.GetKeyDown(KeyCode.UpArrow)) {
+        gravityStrengthX = 0;
+        gravityStrengthY = 1 * globalGravityModifier;
+        UpdateGlobalGravity();
+    } 
+    else if (Input.GetKeyDown(KeyCode.DownArrow)) {
+        gravityStrengthX = 0;
+        gravityStrengthY = -1 * globalGravityModifier;
+        UpdateGlobalGravity();
+    } 
+    else if (Input.GetKeyDown(KeyCode.LeftArrow)) {
+        gravityStrengthX = -1 * globalGravityModifier;
+        gravityStrengthY = 0;
+        UpdateGlobalGravity();
+    } 
+    else if (Input.GetKeyDown(KeyCode.RightArrow)) {
+        gravityStrengthX = 1 * globalGravityModifier;
+        gravityStrengthY = 0;
+        UpdateGlobalGravity();
     }
 }
-    void FreezeX()
-    {
-        rb.constraints = RigidbodyConstraints2D.FreezePositionX;
-        rb.constraints &= ~RigidbodyConstraints2D.FreezePositionY;
-    }
-    void FreezeY()
-    {
-        rb.constraints = RigidbodyConstraints2D.FreezePositionY;
-        rb.constraints &= ~RigidbodyConstraints2D.FreezePositionX;
-    }
+
+void UpdateGlobalGravity() {
+    Physics2D.gravity = new Vector2(gravityStrengthX, gravityStrengthY);
+}
+
 }
