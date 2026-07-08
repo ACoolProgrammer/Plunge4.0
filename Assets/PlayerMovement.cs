5999cbcb8ebcfc6875dbb71d237fd29c79f3be69
0V3RR1D3 <spieringlogan@gmail.com>
2026-07-07 17:58:25 -0700

    reupload

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
@@ -7,13 +8,40 @@ public class PlayerController : MonoBehaviour
     private float gravityStrengthX = 0;
     private float gravityStrengthY = 0;
     public float globalGravityModifier = 1;
+    private CustomInput input = new CustomInput();
+    private Vector2 moveVector = Vector2.zero;
+
     void Start()
     {
         // Get the Rigidbody2D component attached to the player
         rb = GetComponent<Rigidbody2D>();
         rb.gravityScale = 0;
     }
-
+    private void OnEnable()
+    {
+        input.Enable();
+        input.Player.Movement.performed += OnMovementPerformed;
+        input.Player.Movement.canceled += OnMovementCanceled;
+    }
+    private void OnDisable()
+    {
+        input.Disable();
+                input.Player.Movement.performed -= OnMovementPerformed;
+                input.Player.Movement.canceled -= OnMovementCanceled;
+    }
+private void FixedUpdate()
+    {
+        Debug.Log(moveVector);
+    }
+private void OnMovementPerformed(InputAction.CallbackContext value)
+    {
+        moveVector = value.ReadValue<Vector2>();
+    }
+private void OnMovementCanceled(InputAction.CallbackContext value)
+    {
+        moveVector = Vector2.zero;
+    }
+    
     
 void Update() {
     // Updates Global Gravity When Arrow Keys are pressed
